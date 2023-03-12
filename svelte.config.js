1d0c556415b01b5166c8f0583ec247a65911a68b
Blookers <blookyl305@gmail.com>
2023-03-12 15:04:59 +1000

    font caching

--- svelte.config.js
+++ svelte.config.js
@@ -5,17 +5,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	kit: {
-		adapter: adapter()
-	},
+	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.md'],
 	preprocess: [
 		preprocess({}),
 		vitePreprocess(),
 		mdsvex({
-			layout: {
-				guides: 'src/routes/guides/guide.svelte'
-			},
+			layout: { guides: 'src/routes/guides/guide.svelte' },
 			extensions: ['.md']
 		})
 	]
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 export default {
-	define: {
-		'process.env': process.env,
-	},
 	plugins: [sveltekit()]
 } satisfies UserConfig;
